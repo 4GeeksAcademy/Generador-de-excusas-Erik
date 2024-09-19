e488ee369567816949f0220827b266a9c5488990
Erik Martin <erikmartinfernandez@gmail.com>
2024-09-19 17:28:22 +0000

    Entrega

--- src/app.js
+++ src/app.js
@@ -8,4 +8,27 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let who = ["El perro", "La abuela", "La vecina", "El niño", "El gato"];
+  let action = [
+    "se comio un pan",
+    "se murió",
+    "piso al gato",
+    "estudió matematicas",
+    "se bañó"
+  ];
+  let place = [
+    "en la piscina",
+    "en el metro",
+    "en el tren",
+    "en casa",
+    "en el bosque"
+  ];
+
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomPlace = Math.floor(Math.random() * place.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[randomWho] + " " + action[randomAction] + " " + place[randomPlace];
 };
